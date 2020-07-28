f6c4fb35c4ce874d855a732a440cfbfeaf5d275b
alexislusak <alexislusak@gmail.com>
2020-07-27 23:05:53 -0300

    concentracion 3D
    ahora pasa la concentracion de 2D a 3D

--- PSF.py
+++ PSF.py
@@ -15,5 +15,14 @@ def particulasPSF3D(concentracion_molar,volumenPSF=0.18455):
 def particulasPSF2D(moleculas_um2,areaPSF=0.0987):
     return  moleculas_um2*areaPSF
 
-print(particulasPSF3D(concentracion_molar=2.23*10**(-9)))
-print(particulasPSF2D(moleculas_um2=19.2))
+def concentracion3D(moleculas_um2,areaPSF=0.0987,volumenPSF=0.18455):
+    particulasPSC=moleculas_um2*areaPSF
+    micro3alitro=1e-15
+    volumenPSFlitro=volumenPSF*micro3alitro   
+    numeroavogadro=6.022*10**23
+    concentracion_molar= particulasPSC/(volumenPSFlitro*numeroavogadro)
+    return concentracion_molar
+
+moleculas_um2=16    
+print(particulasPSF2D(moleculas_um2))    
+print(concentracion3D(moleculas_um2))
